7f74cdda04fbb19aa50177d3b4a479555c561da3
Vinícius Führ de Borba <vinicius_fuhr-borba@estudante.sc.senai.br>
2021-01-11 14:15:17 -0300

    React Router 6
    Usando react-router-dom para controlar as rotas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import SignIn from './pages/SignIn/'
 
@@ -9,15 +11,16 @@ import theme from './theme'
 
 function App() {
   console.log(window.location.href);
-  const url = window.location.href;
 
   return (
     <ThemeProvider theme={theme}>
-      {
-        url === 'http://localhost:3000/'
-        ? <Home/>
-        : <SignIn/>
-      }
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/sign-in' element={<SignIn />}></Route>
+          <Route path='*' element={<h1>Not Found 404!</h1>}></Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
